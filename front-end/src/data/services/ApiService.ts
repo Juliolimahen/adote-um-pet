6d2b27cc31ab477528237b4c6d4202f48d4118f8
DevPC <DevPC@DEVPC>
2023-02-09 13:36:09 -0300

    fix env var

--- front-end/src/data/services/ApiService.ts
+++ front-end/src/data/services/ApiService.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { BASE_URL } from './Resquest';
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const ApiService = axios.create({
-    baseURL: `${BASE_URL}`,
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
