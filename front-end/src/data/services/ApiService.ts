69b32e0617b1e2d9f8c0a8879d164e91af90fa01
DevPC <DevPC@DEVPC>
2023-02-09 13:20:55 -0300

    fix var

--- front-end/src/data/services/ApiService.ts
+++ front-end/src/data/services/ApiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BASE_URL } from './Resquest';
 
 export const ApiService = axios.create({
-    baseURL: BASE_URL,
+    baseURL: `${BASE_URL}`,
     headers: {
         'Content-Type': 'application/json'
     }
