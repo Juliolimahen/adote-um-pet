c818bcc1a81229dbad42754dc00d0546b712e682
DevPC <DevPC@DEVPC>
2023-02-09 13:45:06 -0300

    fix var

--- front-end/src/data/services/ApiService.ts
+++ front-end/src/data/services/ApiService.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const ApiService = axios.create({
-    baseURL: BASE_URL,
+    baseURL:'https://adote-um-pet-production-131a.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
